5b81b026819e5d6acd3b7887dc4a817c63ba8c4a
Hristijan <chrispetrov904@gmail.com>
2023-12-05 16:42:17 +0100

    added the first left side element ALL

--- todo1.js
+++ todo1.js
@@ -1,6 +1,14 @@
 var x='hello to do list'
 var toDoBody=document.getElementsByTagName("body")[0];
-toDoBody.innerHTML="<div>   <h1>//TO_DO List//</h1>    <div id='body'><div id='left'>left side</div><div id='right'>right side</div></div>    <footer>footer text</footer>    </div>";
+toDoBody.innerHTML="<div>   <h1>//TO_DO List//</h1>    <div id='body'><div id='left'>left side  </div><div id='right'> right side</div></div>    <footer>footer text</footer>    </div>";
+//need to add a left side and right side za da se zapamtat nekoi funkcii
+//prvo ce napravime left side za all tasks
+function leftSide(){
+    var ALL=document.createElement('div');
+    ALL.innerHTML="ALL";
+    document.getElementById('left').innerHTML=ALL.innerHTML;
+}
+leftSide();
 function addText(){
     console.log(toDoBody)
     
