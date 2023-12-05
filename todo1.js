259e20521458b7d9e37e6bff6af073ee44bd456d
Hristijan <chrispetrov904@gmail.com>
2023-12-05 16:17:34 +0100

    added styles

--- todo1.js
+++ todo1.js
@@ -1,7 +1,7 @@
 var x='hello to do list'
-var h1=document.getElementById("2d")
-
+var toDoBody=document.getElementsByTagName("body")[0];
+toDoBody.innerHTML="<div>   <h1>//TO_DO List//</h1>    <div id='body'><div id='left'>left side</div><div id='right'>right side</div></div>    <footer>footer text</footer>    </div>";
 function addText(){
-    console.log("works")
-    h1.innerText=x;
+    console.log(toDoBody)
+    
 }
