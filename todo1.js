bb1c3971a50a233abb1bd33bf3cfd0ad684a15e3
Hristijan <chrispetrov904@gmail.com>
2024-02-16 14:10:53 +0100

    Cleaned up some code and andded the tasks to the left side

--- todo1.js
+++ todo1.js
@@ -3,10 +3,11 @@ var toDoBody=document.getElementsByTagName("body")[0];
 toDoBody.innerHTML="<div>   <h1>//TO_DO List//</h1>    <div id='body'><div id='left'>left side  </div><div id='right'> right side</div></div>    <footer>footer text</footer>    </div>";
 //need to add a left side and right side za da se zapamtat nekoi funkcii
 //prvo ce napravime left side za all tasks
-function leftSide(){
-    var ALL=document.createElement('div');
-    ALL.innerHTML="ALL";
-    document.getElementById('left').innerHTML=ALL.innerHTML;
+function leftSide(){ 
+    var AllItems = document.getElementById('left');
+    AllItems.innerHTML="<button class='nav_button'>TO DO</button></br>";
+    AllItems.innerHTML+="<button class='nav_button'>Projects</button></br>";
+    AllItems.innerHTML+="<button class='nav_button'>Notes</button></br>";
 }
 leftSide();
 function addText(){
